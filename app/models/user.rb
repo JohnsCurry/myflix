efcaa63674e8e64156e23977e5192316939f398e
John Curry <john.steven.curry@gmail.com>
2015-01-02 23:13:57 -0700

    finished week 3

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_many :queue_items, -> { order("position") }
+  has_many :reviews, -> { order("created_at DESC") }
   validates_presence_of :email, :password, :full_name
   validates_uniqueness_of :email
   has_secure_password
@@ -8,5 +9,9 @@ class User < ActiveRecord::Base
     queue_items.each_with_index do |queue_item, index|
       queue_item.update_attributes(position: index + 1)
     end
+  end
+
+  def queued_video?(video)
+    queue_items.map(&:video).include?(video)
   end
 end
